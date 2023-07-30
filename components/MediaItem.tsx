6a399fedd5da3d6f3107e8de4270ca309f0f9b8b
Organique0 <grabnar.luka@gmail.com>
2023-07-30 20:15:48 +0200

    main part done

--- components/MediaItem.tsx
+++ components/MediaItem.tsx
@@ -2,6 +2,7 @@
 
 import useLoadImage from "@/hooks/useLoadImage";
 import { Song } from "@/types";
+import { truncate } from "fs/promises";
 import Image from "next/image";
 
 interface MediaItemProps {
@@ -19,8 +20,8 @@ const MediaItem: React.FC<MediaItemProps> = ({ data, onClick }) => {
     }
     return (
         <div onClick={handleClick} className="flex items-center gap-x-3 cursor-pointer hover:bg-neutral-800/50 w-full p-2 rounded-md">
-            <div className="relative rounded-md min-h-[48px] min-w-[48px] overflow-hidden">
-                <Image fill src={imageUrl || '/images/liked.png'} alt="media item" className="object-cover" />
+            <div className="relative rounded-md min-h-[48px] min-w-[48px] overflow-hidden" >
+                <Image fill src={imageUrl || '/images/liked.png'} alt="media item" className="object-cover" priority={true} />
             </div>
             <div className="flex flex-col gap-y-1 overflow-hidden">
                 <p className="text-white truncate">{data.title}</p>
